ba27918a96268d87cf3cc04e81c34cc4a1b36ee5
Gaurav-dawadi <gaurabdawadi@gamil.com>
2021-01-23 09:18:24 +0545

    feat: setup components for project

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import JokeList from './jokelist'
+
+function App() {
+  return (
+    <div className="App">
+      <div>This is Main Component</div>
+      <JokeList />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/jokelist.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+class JokeList extends React.Component{
+    render(){
+        return(
+            <h1>Dad Jokes</h1>
+        )
+    }
+}
+
+export default JokeList
